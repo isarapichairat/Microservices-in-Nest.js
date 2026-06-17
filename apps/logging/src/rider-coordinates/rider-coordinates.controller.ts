2c6ff0033c4590ac731dc1b21ab75c552e001c47
isara <issarapichairat@gmail.com>
2026-06-17 10:46:14 +0700

    Validating Request Body

--- apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
+++ apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCoordinatesDTO } from './dto/create-coordinates.dto';
 
 @Controller('rider-coordinates')
 export class RiderCoordinatesController {
         @Get()
         getRiderCoordinate(){
         return 'Hello i am from rider coordinate'
+        }
+
+        @Post()
+        saveRiderCoordinates(
+                @Body()
+                createCoordinatesDTO: CreateCoordinatesDTO
+        ){
+                return createCoordinatesDTO
+
+
         }
 }
